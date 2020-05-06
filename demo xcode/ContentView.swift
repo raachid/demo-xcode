959699ed2d795cc47a799d90e5492cb6906f8a83
raachid <rachid533@hotmail.fr>
2020-05-06 11:09:07 +0200

    Initial Commit

---
+++ demo xcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo xcode
+//
+//  Created by Kerbach Rachid on 06/05/2020.
+//  Copyright © 2020 Kerbach Rachid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
